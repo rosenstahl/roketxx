889055b50ef95b5e5b1e9e464e31ec6b084c0c9f
rosenstahl <hasanyilmaz1811@gmail.com>
2025-04-11 22:57:15 +0200

    Import service worker unregistration in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import './styles/global.css'
 import './i18n/config';
 import './styles/fonts.css';
 
+// Service Worker Deregistrierung importieren
+import './unregisterSW.js';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
