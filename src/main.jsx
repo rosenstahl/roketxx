90e2d434aad9ed54b029cc190ef1c13032a942a1
rosenstahl <hasanyilmaz1811@gmail.com>
2025-04-11 23:02:37 +0200

    Remove Service Worker import to fix MIME type error

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,6 @@ import './styles/global.css'
 import './i18n/config';
 import './styles/fonts.css';
 
-// Service Worker Deregistrierung importieren
-import './unregisterSW.js';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
